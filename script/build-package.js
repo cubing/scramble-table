576acf9b257b39b65df21f7331c17c19fda66d7e
Lucas Garron <code@garron.net>
2023-11-02 01:40:53 -0700

    Refactor packaging.

---
+++ script/build-package.js
@@ -0,0 +1,22 @@
+import { build } from "esbuild";
+import { esbuildOptions } from "./esbuildOptions.js";
+
+await build({
+  entryPoints: ["src/bin/encrypt.ts", "src/lib/index.ts"],
+  ...esbuildOptions,
+  bundle: true,
+  target: "es2020",
+  splitting: true,
+  format: "esm",
+  outdir: "dist/",
+  chunkNames: "chunks/[name]-[hash]",
+  sourcemap: true,
+  external: [
+    "array-buffer-to-hex",
+    "cubing",
+    "hex-to-array-buffer",
+    "libsodium-wrappers",
+    "pbkdf2-hmac",
+    "node:*",
+  ],
+});
